3850175e073d7d9342a934fb26661a73ea3e7285
regjsmith <regjsmith@users.noreply.github.com>
2024-07-09 20:37:28 +0100

    Update updateP4V.sh

--- updateP4V.sh
+++ updateP4V.sh
@@ -1,65 +1,69 @@
 #!/usr/bin/bash
-<< 'COMMENT'
-Notes
-Future enhancements for quickly determining latest downloadable version and checking if requested version exists
-
-// In production the latest versions json content comes from
-// https://updates.perforce.com/static/P4V/P4V.json
-
-Defined in //qt/p4v/p24.2/src/gui/web/UpdateWebRequestManager.cpp
-
-List of linux26x86_64 
-
-curl -qs https://updates.perforce.com/static/P4V/P4V.json | jq '.versions[] | select(.platform=="linux26x86_64")'
-
-Example output (truncated):
-
-{"platform": "linux26x86_64",
-  "minor": "4",
-  "major": "2023",
-  "build": "2558838"
-}
-{
-  "platform": "linux26x86_64",
-  "minor": "1",
-  "major": "2024",
-  "build": "2591061"
-}
-COMMENT
 
 # Download and update Linux p4v
 
-# Check the version is supplied as argument in short form like 23.2 
+# Reg Smith
 
-if [[ "$#" -ne 1 ]] || ! [[ "$1" =~ [1-9][0-9]\.[1-9] ]]; then
+now=$(date)
 
-    echo "Please provide version in short form like 23.2"
-    echo "Availble versions are listed at https://ftp.perforce.com/perforce/, with an "r" prefix like r23.2"
-    
+# Fetch list of availble versions that can be dpwnloaded in local file
+
+if [ -f ./P4V.json ]; then
+    mv P4V.json P4V.json.bak
+
+    set -o pipefail
+
+    curl -qs https://updates.perforce.com/static/P4V/P4V.json | jq  -r '.versions[] | select(.platform=="linux26x86_64")| .major+"."+.minor+"."+.build' > P4V.json
+
+    if [ $? -ne 0 ]; then
+        echo Download of https://updates.perforce.com/static/P4V/P4V.json failed
+        exit 1
+    fi
+fi
+
+# List in file is already sorted with the latest being the last line
+latest=$(tail -1 P4V.json | cut -d. -f1-2 | sed 's/^..//')
+
+# Check the version is supplied as argument in short form like 23.2
+# if [[ "$#" -ne 1 ]] || ! [[ "$1" =~ [1-9][0-9]\.[1-9]|latest ]] || ! [[ "$1" == "latest" ]]; then
+if [[ "$#" -ne 1 ]] || ! [[ "$1" =~ [1-9][0-9]\.[1-9]|latest ]] ; then
+    echo "Please provide version in short form like $latest"
+    echo ""
+    echo "As of $now:"
+    echo ""
+    echo " - Latest version is $latest"
+    echo " - Currently availble versions are availble for download from https://ftp.perforce.com/perforce/, with an "r" prefix like r23.2"
+    echo " - Full list with build numbers extracted from https://updates.perforce.com/static/P4V/P4V.json are as follows:"
+    echo ""
+    cat P4V.json
+
     # Notes for later improvements
-
-    # - To get a list of availble versions in short form run one of these
+    # - To get a list of availble versionsin short form run one of these
     #   (the grep  comes out faster according t "time")
+
     #   curl -s https://ftp.perforce.com/perforce/ | sed -n 's#.* href="r\([1-9][0-9]\.[1-9]\)/.*#\1#p'
     #   curl -s https://ftp.perforce.com/perforce/ | grep -Po '(?<= href="r)[1-9]{2}\.[1-9](?=/)'
 
     # - To look for p4v.tgz files under any of the returned version numbers, e.g. 24.1 (useful for checking for latest)
     #   curl -s  https://ftp.perforce.com/perforce/r24.1/bin.linux26x86_64/ -o - | grep -Po '(?<= href=")p4v.tgz(?=")'
-    
+
     exit
 else
     version=$1
 fi
 
-# Run a command using sudo so it only asks for password at the start and won't 
+# testing code thus far
+#exit
 
+# Run a command using sudo so it only asks for password at the start and won't
 # ask again while rest of script runs, default for sudo is to store password for 15 minutes
 
 sudo echo Installing/updating P4V
 
+
 if [ -f /opt/perforce/bin/p4v/bin/p4v ];then
     installedVersion=$(/opt/perforce/bin/p4v/bin/p4v -V | grep "^Rev"| cut -d/ -f3,4 | sed 's#/#.#')
-    echo Currently installed version P4V $installedVersion
+    echo Currently installed version of P4V $installedVersion
 else
     echo "No currently installed P4V found at /opt/perforce/bin/p4v/bin/p4v"
 fi
@@ -67,9 +71,8 @@ fi
 # Look for already downloaded p4v.tgz in current directory, else determine online version
 
 if [ -f ./p4v.tgz ]; then
-    echo "Found p4v.tgz in current directory, using that"
-    
     localtarVersion=$(tar -ztf p4v.tgz | head -1 | sed 's#/$##' | cut -d- -f2-)
+    echo "Found p4v.tgz in current directory for P4V version $localtarVersion, using that for install"
 
     if [ "$localtarVersion" = "$installedVersion" ]; then
         echo "Local p4v.tgz version matches currently installed P4V $installedVersion ... exiting"
@@ -77,10 +80,10 @@ if [ -f ./p4v.tgz ]; then
     fi
 else
     # Check if we already have the same version as availble for download installed.
-    # Download a small amount of the tar file to check version, clumsy (it's a zipped tar file and we only get 
+    # Download a small amount of the tar file to check version, clumsy (it's a zipped tar file and we only get
     # the first 256 bytes and it does give errors about not being valid archive, but seems to work
-
     # Determine version in tarfile by inspecting first line of tar file listing
+
     downloadableVersion=$(curl -s -r 0-256 https://ftp.perforce.com/perforce/r$version/bin.linux26x86_64/p4v.tgz -o - | tar -ztf - 2>/dev/null | head -1 | sed 's#/$##' | cut -d- -f2-)
 
     if [[ "$downloadableVersion" = "$installedVersion" ]]; then
@@ -88,12 +91,17 @@ else
         exit
     fi
 
-    # clean up our partial download
-    rm p4v.tgz
+    # Clean up our partial download if it exists
+    if [ -f ./p4v.tgz ]; then
+        rm p4v.tgz
+    fi
 
-# Installed version not the available download version so download full tgz to current directory
-# Currently hardwired to a particluar version (23.4 below), wonder if there is a /latest link?
-curl https://ftp.perforce.com/perforce/r$version/bin.linux26x86_64/p4v.tgz -o p4v.tgz
+
+    # Installed version not the available download version so download full tgz to current directory
+    # Currently hardwired to a particluar version (23.4 below), wonder if there is a /latest link?
+
+    echo "Downloading https://ftp.perforce.com/perforce/r$version/bin.linux26x86_64/p4v.tgz"
+    curl https://ftp.perforce.com/perforce/r$version/bin.linux26x86_64/p4v.tgz -o p4v.tgz
 
 fi
 
@@ -106,7 +114,7 @@ fi
 sudo mkdir /opt/perforce/bin/p4v
 sudo tar -xvf p4v.tgz -C /opt/perforce/bin/p4v --strip-components=1
 
-# Create or replace link to /usr/local/bin/p4v 
+# Create or replace link to /usr/local/bin/p4v
 if [ -f /usr/local/bin/p4v ]; then
     sudo rm /usr/local/bin/p4v
 fi
@@ -115,6 +123,4 @@ sudo ln -s /opt/perforce/bin/p4v/bin/p4v /usr/local/bin/p4v
 
 echo Created link /opt/perforce/bin/p4v/bin/p4v to /usr/local/bin/p4v
 echo Updated P4V to following version:
-
 p4v -V
-
